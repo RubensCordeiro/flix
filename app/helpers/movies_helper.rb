04641b61da832eba8e3387c968873e719bba41f2
Rubens Cordeiro <rubensccordeiro@outlook.com>
2023-11-20 19:49:46 -0300

    feat: add helper methods for movie index view

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,11 @@
+# frozen_string_literal: true
+
 module MoviesHelper
+  def gross_amount(movie)
+    movie.flop? ? "Flop!!" : number_to_currency(movie.gross_amount, precision: 0)
+  end
+
+  def release_date(movie)
+    movie.released_on.strftime("%d/%m/%Y")
+  end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 class Movie < ApplicationRecord
+  def flop?
+    gross_amount <= 250E6
+  end
 end
