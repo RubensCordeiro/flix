d4888f6591bd6fe9124a6ce88c94f43901dbc542
Rubens Cordeiro <rubensccordeiro@outlook.com>
2023-11-21 11:11:45 -0300

    feat: add basic styling to the app

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,8 +1,12 @@
 # frozen_string_literal: true
 
 module MoviesHelper
+  def currency_to_millions(amount)
+    (amount.to_f / 1E6).round(0)
+  end
+
   def gross_amount(movie)
-    movie.flop? ? "Flop!!" : number_to_currency(movie.gross_amount, precision: 0)
+    movie.flop? ? "Flop!!" : "#{currency_to_millions(movie.gross_amount)} M"
   end
 
   def release_date(movie)
