790158037cd5e6a5d6415e6dd222a279408b45e1
Rubens Cordeiro <rubensccordeiro@outlook.com>
2023-11-24 10:36:07 -0300

    feat: started edit form

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@
 
 Rails.application.routes.draw do
   get "movies", to: "movies#index"
-  get "movies/:id", to: "movies#show"
+  get "movies/:id", to: "movies#show", as: "movie"
+  get "movies/:id/edit", to: "movies#edit", as: "edit_movie"
 end
